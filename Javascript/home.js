da394322ef941de7b127cad9855287f508fa1210
ErwinDelosReyes <hfjwuaahhqu@gmail.com>
2025-01-31 16:29:49 +0800

    Update home.js

--- Javascript/home.js
+++ Javascript/home.js
@@ -218,7 +218,7 @@ fileInput.addEventListener('change', async (event) => {
     formData.append('parent_id', parentId);
 
     try {
-        const response = await fetch('http://localhost:3000/upload', {
+        const response = await fetch('https://dvd-net102-2.onrender.com/upload', {
             method: 'POST',
             body: formData,
         });
